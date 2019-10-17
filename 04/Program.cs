368df88b6e86f782d6266791df9b225b2a09408e
filip šupolík <filipsupolik25@gmail.com>
2019-10-17 12:25:44 +0200

    Completed OpenLab - 02.4

---
+++ 04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04
+{
+    class Program
+    {
+        public static int GetLegsCount(int chickens, int cows, int pigs)
+        {
+            return (chickens * 2 + cows * 4 + pigs * 4);
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
